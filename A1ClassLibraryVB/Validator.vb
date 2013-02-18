4d84c3b950fb60c7286e55d1278778994492ab5b
Stanley Tsang <stsang1@learn.senecac.on.ca>
2013-02-18 17:10:43 -0500

    added additional tests, coded out some of the Validator class

--- A1ClassLibraryVB/Validator.vb
+++ A1ClassLibraryVB/Validator.vb
@@ -2,17 +2,18 @@
 
 Public Class Validator
 
-    Dim userNameExpression
-    Dim emailExpression
+    Dim userNameExpression As New System.Text.RegularExpressions.Regex("[^a-zA-z0-9]+")
+    Dim emailFormatExpression1 As New System.Text.RegularExpressions.Regex("^([0-9a-zA-Z]+@myseneca.ca)$")
+    Dim emailFormatExpression2 As New System.Text.RegularExpressions.Regex("^([0-9a-zA-Z]([.]{0,1}[0-9a-zA-Z])*@senecacollege.on.ca)$")
     Dim phoneExpression
     Dim addressExpression
 
     Public Function validateUserName(ByVal value As String) As Boolean
-        Return True
+        Return Not userNameExpression.IsMatch(value)
     End Function
 
     Public Function validateEmail(ByVal value As String) As Boolean
-        Return True
+        Return emailFormatExpression1.IsMatch(value) Or emailFormatExpression2.IsMatch(value)
     End Function
 
     Public Function validatePhone(ByVal value As String) As Boolean
--- UnitTestProject1/ValidatorTest.vb
+++ UnitTestProject1/ValidatorTest.vb
@@ -1,9 +1,58 @@
 ﻿Imports System.Text
 Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports A1ClassLibraryVB
 
 <TestClass()> Public Class ValidatorTest
 
-    <TestMethod()> Public Sub TestMethod1()
+    Dim testValidator As Validator
+
+    ' called before each test case
+    <TestInitialize()> Public Sub Initialize()
+        testValidator = New Validator()
     End Sub
 
+    ' called after each test case
+    <TestCleanup()> Public Sub CleanUp()
+        testValidator = Nothing
+    End Sub
+
+    <TestMethod()> Public Sub TestUserNameValidationGood()
+        Assert.IsTrue(testValidator.validateUserName("stsang1"))
+    End Sub
+
+    <TestMethod()> Public Sub TestUserNameValidationBad()
+        Assert.IsFalse(testValidator.validateUserName("@st 541"))
+    End Sub
+
+    <TestMethod()> Public Sub TestEmailFormat1ValidationGood()
+        Assert.IsTrue(testValidator.validateEmail("stsang1@myseneca.ca"))
+    End Sub
+
+    <TestMethod()> Public Sub TestEmailFormat1ValidationBad()
+        Assert.IsFalse(testValidator.validateEmail("stsang1-bad@senecacollege.on.ca"))
+    End Sub
+
+    <TestMethod()> Public Sub TestEmailFormat2ValidationGood()
+        Assert.IsTrue(testValidator.validateEmail("stanley.tsang@senecacollege.on.ca"))
+    End Sub
+
+    <TestMethod()> Public Sub TestEmailFormat2ValidationBad()
+        Assert.IsFalse(testValidator.validateEmail("stsang1-bad@myseneca.on.ca"))
+    End Sub
+
+    <TestMethod()> Public Sub TestPhoneNumberValidationGood()
+        Assert.IsTrue(True)
+    End Sub
+
+    <TestMethod()> Public Sub TestPhoneNumberValidationBad()
+        Assert.IsFalse(False)
+    End Sub
+
+    <TestMethod()> Public Sub TestAddressValidationGood()
+        Assert.IsTrue(True)
+    End Sub
+
+    <TestMethod()> Public Sub TestAddressValidationBad()
+        Assert.IsFalse(False)
+    End Sub
 End Class
