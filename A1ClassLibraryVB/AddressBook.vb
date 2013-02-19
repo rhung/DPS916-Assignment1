1441a6d5cef7f413771930f9ff78790f46cb37c6
Stanley Tsang <stsang1@learn.senecac.on.ca>
2013-02-18 19:35:14 -0500

    coded out JSON functions, added tests, fixed sorting records in AddressBook

--- A1ClassLibraryVB/AddressBook.vb
+++ A1ClassLibraryVB/AddressBook.vb
@@ -1,17 +1,18 @@
 ﻿Imports A1ClassLibraryCS
+Imports Newtonsoft.Json
+Imports System.Linq
 
 Public Class AddressBook
     Implements IJSONSerializer
 
-    Dim fileName As String
-    Dim recordsList As New ArrayList()
+    Dim recordsList As New List(Of RecordA1)
 
     Public Event createSuccessEvent()
     Public Event updateSuccessEvent()
     Public Event deleteSuccessEvent()
     Public Event getSuccessEvent()
 
-    Public ReadOnly Property Records() As ArrayList
+    Public ReadOnly Property Records() As List(Of RecordA1)
         Get
             Return recordsList
         End Get
@@ -19,7 +20,7 @@ Public Class AddressBook
 
     Public Sub createRecord(ByVal record As RecordA1)
         recordsList.Add(record)
-        recordsList.Sort()
+        recordsList = recordsList.OrderBy(Function(x) x.UserName).ToList()
         RaiseEvent createSuccessEvent()
     End Sub
 
@@ -33,23 +34,60 @@ Public Class AddressBook
         RaiseEvent deleteSuccessEvent()
     End Sub
 
-    Public Function getAllUserNames() As ArrayList
-        Dim userNameList As New ArrayList
+    Public Function getAllUserNames() As List(Of String)
+        Dim userNameList As New List(Of String)
         For Each record As RecordA1 In recordsList
             userNameList.Add(record.UserName)
         Next
+        RaiseEvent getSuccessEvent()
         Return userNameList
     End Function
 
     Public Function readFromJSON(ByVal file As String) As Boolean Implements IJSONSerializer.readFromJSON
         Dim result As Boolean = True
-        ' Insert code here
+        Using readFile As New IO.StreamReader(file)
+            Try
+                Dim jsonString As String = readFile.ReadToEnd
+                Dim records As List(Of RecordA1) = JsonConvert.DeserializeObject(Of List(Of RecordA1))(jsonString)
+                Me.recordsList = records
+            Catch ex As Exception
+                result = False
+            End Try
+        End Using
         Return result
     End Function
 
     Public Function writeToJSON(file As String) As Boolean Implements IJSONSerializer.writeToJSON
         Dim result As Boolean = True
-        ' Insert code here
+        Using writeFile As New IO.StreamWriter(file)
+            Try
+                Dim jsonString = JsonConvert.SerializeObject(Me.recordsList, Formatting.Indented)
+                writeFile.Write(jsonString)
+            Catch ex As Exception
+                result = False
+            End Try
+        End Using
         Return result
+    End Function
+
+    Private Shared Function CompareRecordsByUserName(ByVal record1 As RecordA1, ByVal record2 As RecordA1)
+        If record1.UserName Is Nothing Then
+            If record2.UserName Is Nothing Then
+                ' If both user names nothing then equal
+                Return 0
+            Else
+                ' If record1's userName is nothing and record2's userName not nothing, then record2 is greater
+                Return -1
+            End If
+
+        Else
+            ' If record2's userName is not nothing and record2's userName is nothing, then record1 is greater
+            If record2.UserName Is Nothing Then
+                Return 1
+            Else
+                ' If both are not nothing, compare them and return result
+                Return record1.UserName.CompareTo(record2.UserName)
+            End If
+        End If
     End Function
 End Class
--- UnitTestProject1/AddressBookTest.vb
+++ UnitTestProject1/AddressBookTest.vb
@@ -1,9 +1,120 @@
 ﻿Imports System.Text
 Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports A1ClassLibraryVB
+Imports A1ClassLibraryCS
 
 <TestClass()> Public Class UnitTest1
 
-    <TestMethod()> Public Sub TestMethod1()
+    Dim eventThrown As Boolean
+    Dim WithEvents testAddressBook As AddressBook
+
+    ' called before each test case
+    <TestInitialize()> Public Sub Initialize()
+        testAddressBook = New AddressBook()
+        Dim record1 As New RecordA1()
+        record1.UserName = "stsang5"
+        record1.EmailAddresses.Add("stsang5@myseneca.ca")
+        testAddressBook.createRecord(record1)
     End Sub
 
+    ' called after each test case
+    <TestCleanup()> Public Sub CleanUp()
+        eventThrown = False
+        testAddressBook = Nothing
+    End Sub
+
+    <TestMethod()> Public Sub TestCreateRecord()
+        Assert.IsTrue(eventThrown)
+    End Sub
+
+    <TestMethod()> Public Sub TestUpdateRecord()
+        eventThrown = False
+        Dim record2 As New RecordA1()
+        record2.UserName = "stsang2"
+        record2.EmailAddresses.Add("stsang2@senecacollege.on.ca")
+        testAddressBook.updateRecord(0, record2)
+        Assert.IsTrue(eventThrown)
+        Assert.AreEqual("stsang2", testAddressBook.Records(0).UserName)
+    End Sub
+
+    <TestMethod()> Public Sub TestDeleteRecord()
+        eventThrown = False
+        testAddressBook.deleteRecord(0)
+        Assert.IsTrue(eventThrown)
+        Assert.AreEqual(0, testAddressBook.Records.Count)
+    End Sub
+
+    <TestMethod()> Public Sub TestGetRecords()
+        Dim record2 As New RecordA1()
+        record2.UserName = "stsang2"
+        record2.EmailAddresses.Add("stsang2@senecacollege.on.ca")
+        testAddressBook.createRecord(record2)
+        eventThrown = False
+        Dim userNameList As List(Of String) = testAddressBook.getAllUserNames()
+        Assert.IsTrue(eventThrown)
+        Assert.AreEqual(2, userNameList.Count)
+    End Sub
+
+    ' Test that sorting works
+    <TestMethod()> Public Sub TestRecordSorting()
+        Dim record2 As New RecordA1()
+        record2.UserName = "stsang2"
+        record2.EmailAddresses.Add("stsang2@senecacollege.on.ca")
+        testAddressBook.createRecord(record2)
+        Dim record3 As New RecordA1()
+        record3.UserName = "stsang3"
+        record3.EmailAddresses.Add("stsang3@senecacollege.on.ca")
+        testAddressBook.createRecord(record3)
+        Assert.AreEqual("stsang2", testAddressBook.Records(0).UserName)
+        Assert.AreEqual("stsang2@senecacollege.on.ca", testAddressBook.Records(0).EmailAddresses(0))
+    End Sub
+
+    ' Test that JSONSerializer can write
+    <TestMethod()> Public Sub TestWriteTOJSON()
+        Dim record2 As New RecordA1()
+        record2.UserName = "stsang2"
+        record2.EmailAddresses.Add("stsang2@senecacollege.on.ca")
+        testAddressBook.createRecord(record2)
+        Dim record3 As New RecordA1()
+        record3.UserName = "stsang3"
+        record3.EmailAddresses.Add("stsang3@senecacollege.on.ca")
+        testAddressBook.createRecord(record3)
+        Dim result As Boolean = testAddressBook.writeToJSON("test.txt")
+        Assert.IsTrue(result)
+    End Sub
+
+    ' Test that JSONSerializer can read
+    <TestMethod()> Public Sub TestReadFromJSON()
+        Dim testAddressBook2 As New AddressBook()
+        Dim result As Boolean = testAddressBook2.readFromJSON("test.txt")
+        Assert.IsTrue(result)
+        Assert.AreEqual("stsang2", testAddressBook2.Records(0).UserName)
+        Assert.AreEqual("stsang2@senecacollege.on.ca", testAddressBook2.Records(0).EmailAddresses(0))
+    End Sub
+
+    ' Test that JSONSerializer can read
+    <TestMethod(), ExpectedException(GetType(System.IO.FileNotFoundException))> Public Sub TestReadFromJSONGoodInvalidFile()
+        Dim testAddressBook2 As New AddressBook()
+        testAddressBook2.readFromJSON("fake.txt")
+    End Sub
+
+    ' createSuccess Event handler - turns global variable eventThrown into true
+    Private Sub TestAddressBook_createSuccessEvent() Handles testAddressBook.createSuccessEvent
+        eventThrown = True
+    End Sub
+
+    ' createSuccess Event handler - turns global variable eventThrown into true
+    Private Sub TestAddressBook_updateSuccessEvent() Handles testAddressBook.updateSuccessEvent
+        eventThrown = True
+    End Sub
+
+    ' createSuccess Event handler - turns global variable eventThrown into true
+    Private Sub TestAddressBook_deleteSuccessEvent() Handles testAddressBook.deleteSuccessEvent
+        eventThrown = True
+    End Sub
+
+    ' createSuccess Event handler - turns global variable eventThrown into true
+    Private Sub TestAddressBook_getSuccessEvent() Handles testAddressBook.getSuccessEvent
+        eventThrown = True
+    End Sub
 End Class
